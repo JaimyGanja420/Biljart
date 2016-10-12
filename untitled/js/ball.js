54c735901876608ecc7e63159d5e8f66fed24b51
JaimyGanja420 <jaimybeima@live.nl>
2016-10-12 12:18:09 +0200

    Alluha Akbar :camel:  :fire:

--- untitled/js/ball.js
+++ untitled/js/ball.js
@@ -6,9 +6,11 @@
 
 var geometry = new THREE.SphereGeometry(0.5,16,8);
 var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-var sphere = new THREE.Mesh( geometry, material );
+var whiteSphere = new THREE.Mesh( geometry, material );
+whiteSphere.speedx = "OH SHIT HERE COME THAT BOI";
+whiteSphere.speedz = 0;
 
 
 var geometry2 = new THREE.SphereGeometry(0.5,16,8);
 var material2 = new THREE.MeshBasicMaterial( {color: 0xff0000} );
-var red = new THREE.Mesh( geometry2, material2 );
+var redSphere = new THREE.Mesh( geometry2, material2 );
