957e2ec33586aa816b539df63af6e2314932adaf
Frank Keuning <Frank_keuning@hotmail.com>
2016-10-12 12:52:43 +0200

    properties

--- untitled/js/ball.js
+++ untitled/js/ball.js
@@ -7,10 +7,34 @@
 var geometry = new THREE.SphereGeometry(0.5,16,8);
 var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
 var whiteSphere = new THREE.Mesh( geometry, material );
-whiteSphere.speedx = "OH SHIT HERE COME THAT BOI";
-whiteSphere.speedz = 0;
+Object.defineProperty(whiteSphere, 'speedX', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
 
+Object.defineProperty(whiteSphere, 'speedZ', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
 
 var geometry2 = new THREE.SphereGeometry(0.5,16,8);
 var material2 = new THREE.MeshBasicMaterial( {color: 0xff0000} );
 var redSphere = new THREE.Mesh( geometry2, material2 );
+
+Object.defineProperty(redSphere, 'speedX', {
+   value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+Object.defineProperty(redSphere, 'speedZ', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
