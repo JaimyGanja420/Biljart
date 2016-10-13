45d138006c38b033cf0b20601fe5a4b6d4e22d83
JaimyGanja420 <jaimybeima@live.nl>
2016-10-13 13:29:29 +0200

    Dank not working collision.

--- untitled/js/Physics.js
+++ untitled/js/Physics.js
@@ -2,8 +2,8 @@
  * Created by JaimyBeima on 11-10-2016.
  */
 
-function collide (Ball1, Ball2) {
-    if (Ball1.bbox.box.isIntersectionBox(Ball2.bbox.box)){
+function collide (Box1, Box2, Ball1, Ball2) {
+    if (Box1.box.isIntersectionBox(Box2.box)){
         //Speed transfer
         tempx = Ball1.SpeedX;
         Ball1.SpeedX = Ball2.SpeedX;
@@ -15,27 +15,25 @@ function collide (Ball1, Ball2) {
     }
 }
 
-function collideWall (Ball1, Ball2){
-    if(Ball1.position.x >= 6.5 || Ball1.position.x <= -6.5){
-        whiteBall.speedX *= -1;
+function collideWall (Ball){
+    if(Ball.position.x >= 6.5 || Ball.position.x <= -6.5){
+        Ball1.speedX *= -1;
     }
 
-    if(Ball1.position.z >= 11.75 || Ball1.position.z <= -11.75){
-        whiteBall.speedZ *= -1;
-    }
-
-    if(Ball2.position.x >= 6.5 || Ball2.position.x <= -6.5){
-        redBall1.speedX *= -1;
-    }
-
-    if(Ball2.position.z >= 11.75 ||Ball2.position.z <= -11.75){
-        redBall1.speedZ *= -1;
+    if(Ball.position.z >= 11.75 || Ball.position.z <= -11.75){
+        Ball.speedZ *= -1;
     }
 }
 
 function slowDown (Ball){
-    if(Ball.speedX > 0 || Ball.speedZ > 0){
+    if(Ball.speedX > 0 || Ball.speedZ > 0 || Ball.speedX < 0 || Ball.speedZ < 0){
         Ball.speedX *= 0.980;
         Ball.speedZ *= 0.980;
+    }
+    if(Ball.speedX >= -0.0005 && Ball.speedX <= 0.0005){
+        Ball.speedX = 0;
+    }
+    if(Ball.speedZ >= -0.0005 && Ball.speedZ <= 0.0005){
+        Ball.speedZ = 0;
     }
 }
