791f17f4e67f26a614fc4b3aeea954946179dd69
boon1502 <boon1502@student.nhl.nl>
2016-10-13 15:12:18 +0200

    pls work

--- untitled/js/Physics.js
+++ untitled/js/Physics.js
@@ -2,40 +2,41 @@
  * Created by JaimyBeima on 11-10-2016.
  */
 
-function collide (Ball1, Ball2) {
-    if (Ball1.bbox.box.isIntersectionBox(Ball2.bbox.box)){
+function collide (Ball1, Ball2, box1, box2) {
+    if (box1.box.intersectsBox(box2.box)){
+        console.log("collision");
         //Speed transfer
-        tempx = Ball1.SpeedX;
-        Ball1.SpeedX = Ball2.SpeedX;
+       // Ball1.speedX *= -1;
+       // Ball1.speedZ *= -1;
+        tempx = Ball1.speedX;
+        Ball1.speedX = Ball2.speedX;
         Ball2.SpeedX = tempx;
 
-        tempz = Ball1.SpeedZ;
-        Ball1.SpeedZ = Ball2.SpeedZ;
-        Ball2.SpeedZ = tempz;
+        tempz = Ball1.speedZ;
+        Ball1.speedZ = Ball2.speedZ;
+        Ball2.speedZ = tempz;
     }
 }
 
-function collideWall (Ball1, Ball2){
+function collideWall (Ball1){
     if(Ball1.position.x >= 6.5 || Ball1.position.x <= -6.5){
-        whiteBall.speedX *= -1;
+        Ball1.speedX *= -1;
     }
 
-    if(Ball1.position.z >= 11.75 || Ball1.position.z <= -11.75){
-        whiteBall.speedZ *= -1;
-    }
-
-    if(Ball2.position.x >= 6.5 || Ball2.position.x <= -6.5){
-        redBall1.speedX *= -1;
-    }
-
-    if(Ball2.position.z >= 11.75 ||Ball2.position.z <= -11.75){
-        redBall1.speedZ *= -1;
+    else if(Ball1.position.z >= 11.75 || Ball1.position.z <= -11.75){
+        Ball1.speedZ *= -1;
     }
 }
 
 function slowDown (Ball){
-    if(Ball.speedX > 0 || Ball.speedZ > 0){
+    if(Ball.speedX > 0 || Ball.speedZ > 0 || Ball.speedX < 0 || Ball.speedZ < 0){
         Ball.speedX *= 0.980;
         Ball.speedZ *= 0.980;
+    }
+    if(Ball.speedX >= -0.00005 && Ball.speedX <= 0.00005){
+        Ball.speedX = 0;
+    }
+    if(Ball.speedZ >= -0.0005 && Ball.speedZ <= 0.00005){
+        Ball.speedZ = 0;
     }
 }
