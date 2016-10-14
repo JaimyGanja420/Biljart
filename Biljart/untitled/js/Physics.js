5a63d41aa5bbe5429afef4865a9b24634165971d
Frank Keuning <Frank_keuning@hotmail.com>
2016-10-14 12:00:58 +0200

    :camel: :bomb:

--- Biljart/untitled/js/Physics.js
+++ Biljart/untitled/js/Physics.js
@@ -15,6 +15,31 @@ function collide (Ball1, Ball2, box1, box2) {
         tempz = Ball1.speedZ;
         Ball1.speedZ = Ball2.speedZ;
         Ball2.speedZ = tempz;
+
+
+    }
+}
+
+function newCollide(ball1, ball2)
+{
+    a = ball1.position.x - ball2.position.x;
+    b = ball1.position.z - ball2.position.z;
+    ab = Math.sqrt((a * a) + (b * b));
+
+    if(ab < 1)
+    {
+        //sphere en sphere2 collide
+        /*ball1.speedX = ((ball1.speedX * (1-1)+(2*1*ball2.speedX))/(1+1));
+        ball1.speedZ = ((ball1.speedZ * (1-1)+(2*1*ball2.speedZ))/(1+1));
+        ball2.speedX = ((ball2.speedX * (1-1)+(2*1*ball1.speedX))/(1+1));
+        ball2.speedZ = ((ball2.speedZ * (1-1)+(2*1*ball1.speedZ))/(1+1));*/
+        tempx = ball1.speedX;
+        ball1.speedX = ball2.speedX;
+        ball2.SpeedX = tempx;
+
+        tempz = ball1.speedZ;
+        ball1.speedZ = ball2.speedZ;
+        ball2.speedZ = tempz;
     }
 }
 
