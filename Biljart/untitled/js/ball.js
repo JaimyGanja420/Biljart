2eb90cdb44a6c9a2e8f2a6f574c22aa62bc067d1
boon1502 <boon1502@student.nhl.nl>
2016-10-14 19:36:06 +0200

    alle ballen

--- Biljart/untitled/js/ball.js
+++ Biljart/untitled/js/ball.js
@@ -38,7 +38,7 @@ Object.defineProperty(whiteSphere, 'speed', {
 });*/
 
 //Redball
-var geometry2 = new THREE.SphereGeometry(0.5,16,8);
+var geometry2 = new THREE.SphereGeometry(0.5,32,32);
 var material2 = new THREE.MeshBasicMaterial( {color: 0xff0000} );
 var redSphere = new THREE.Mesh( geometry2, material2 );
 redBox = new THREE.BoundingBoxHelper(redSphere,0x00ff0f);
@@ -292,3 +292,129 @@ Object.defineProperty(redSphere10, 'speed', {
     enumerable: false,
     configurable: true
 });
+
+var redSphere11 = new THREE.Mesh( geometry2, material2 );
+redBox = new THREE.BoundingBoxHelper(redSphere,0x00ff0f);
+redSphere.bbox = redBox;
+
+Object.defineProperty(redSphere11, 'directionX', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+Object.defineProperty(redSphere11, 'directionZ', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+Object.defineProperty(redSphere11, 'speed', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+var redSphere12 = new THREE.Mesh( geometry2, material2 );
+redBox = new THREE.BoundingBoxHelper(redSphere,0x00ff0f);
+redSphere.bbox = redBox;
+
+Object.defineProperty(redSphere12, 'directionX', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+Object.defineProperty(redSphere12, 'directionZ', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+Object.defineProperty(redSphere12, 'speed', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+var redSphere13 = new THREE.Mesh( geometry2, material2 );
+redBox = new THREE.BoundingBoxHelper(redSphere,0x00ff0f);
+redSphere.bbox = redBox;
+
+Object.defineProperty(redSphere13, 'directionX', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+Object.defineProperty(redSphere13, 'directionZ', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+Object.defineProperty(redSphere13, 'speed', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+var redSphere14 = new THREE.Mesh( geometry2, material2 );
+redBox = new THREE.BoundingBoxHelper(redSphere,0x00ff0f);
+redSphere.bbox = redBox;
+
+Object.defineProperty(redSphere14, 'directionX', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+Object.defineProperty(redSphere14, 'directionZ', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+Object.defineProperty(redSphere14, 'speed', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+var redSphere15 = new THREE.Mesh( geometry2, material2 );
+redBox = new THREE.BoundingBoxHelper(redSphere,0x00ff0f);
+redSphere.bbox = redBox;
+
+Object.defineProperty(redSphere15, 'directionX', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+Object.defineProperty(redSphere15, 'directionZ', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+Object.defineProperty(redSphere15, 'speed', {
+    value: 0,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
